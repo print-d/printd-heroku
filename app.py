8e2b945e97f43d566d7d4f1cbc0f7d5db07b8ea0
Adam Tremonte <abtremonte@crimson.ua.edu>
2017-04-01 15:22:10 -0500

    Testing post method

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
 import os
+import psycopg2
+import requests
+import urlparse
+import uuid
 from flask import *
+
+urlparse.uses_netloc.append("postgres")
+URL = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+CONN = psycopg2.connect(
+    database=URL.path[1:],
+    user=URL.username,
+    password=URL.password,
+    host=URL.hostname,
+    port=URL.port
+)
+
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Testing!'
+
+@app.route('/test', methods=['POST'])
+def generate_user():
+	return 'This is a test post'
+	
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
