6051f19505ca69442e1cbc74775224f20e6e7be0
Adam Tremonte <abtremonte@crimson.ua.edu>
2017-04-01 14:30:00 -0500

    testing...

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello world!'
+    return 'Testing!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
