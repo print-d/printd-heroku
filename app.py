4dc7708ddc3b0b4c5f933ff018cf0c76a4f53289
Adam Tremonte <abtremonte@crimson.ua.edu>
2017-04-04 16:18:14 -0500

    Testing hashing

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	hash = pbkdf2_sha256.hash("testpassword")
+	hash = pbkdf2_sha256.hash("turtle1")
 	return hash
 
 @app.route('/test', methods=['POST'])
